f614a238f4f0160b4c137253885dfe1f9f3e2496
Poobest Jirathitiphuwadon <>
2018-03-14 15:59:34 +0700

    get and post methods

--- app.js
+++ app.js
@@ -9,10 +9,32 @@ Menu = require('./models/menu');
 
 mongoose.connect('mongodb://localhost:27017/menu');
 
+var db = mongoose.connection;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
 app.get('/', function(req, res){
-    res.end('hello world');
+    // res.end('hello world');
+    Menu.getMenu(function(err, menu){
+		if(err){
+			throw err;
+		}
+		res.json(menu);
+	});
 });
 
+app.post('/post/menu', function(req, res){
+	var menu = req.body;
+	Menu.addMenu(menu, function(err, menu){
+		if(err){
+			throw err;
+		}
+		res.json(menu);
+	});
+});
 
 var server = app.listen(2222, function(req, res){
     console.log('server start port : 2222');
--- models/menu.js
+++ models/menu.js
@@ -4,10 +4,22 @@ var Schema = mongoose.Schema;
 var menuSchema = mongoose.Schema({
     "name":{
         type:String
-    }
+    },
+    "nameThai":{
+        type:String
+    },
+    "description":{
+        type:String
+    },
+    "ingredient":{
+        type:String
+    },
+    "imgUrl":{
+        type:String
+    },
 });
 
-var Menu = module.exports = mongoose.model('Menu', menuSchema);
+var Menu = module.exports = mongoose.model('menu', menuSchema);
 
 module.exports.getMenu = function(callback, limit){
 	Menu.find(callback).limit(limit);
